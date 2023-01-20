22381805513ce663d1fce81ca217c65baa8063fc
Hasnain-js <ihasnain4@gmail.com>
2023-01-20 16:18:43 +0500

    add: style loader in app

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,17 @@ module.exports = {
                     loader: 'swc-loader',
                 }
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
         ],
     },
     plugins: [
